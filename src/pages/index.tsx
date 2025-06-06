49aca562fc7ddc092f338f0a8ff426db3267174b
Jenorg <jenfermz44@gmail.com>
2025-06-05 19:42:05 -0500

    feat: :tada: Hola mundo!

---
+++ src/pages/index.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+    <div
+      className="flex flex-col items-center justify-center min-h-screen"
+    >
+      <main className="grid grid-cols-1 place-items-center h-full">
+        <h2 className="">Bienvenidos al repositorio!</h2>
+        {/* aqui abajo escriben el hola mundo 
+          cada quien una sola letra, hasta el espacio cuenta!
+          <h1 className="text-4xl">Hola mundo!</h1> 
+        */}
+        <h1 className="text-4xl"></h1> 
+      </main>
+    </div>
+  );
+}
