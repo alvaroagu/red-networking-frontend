b339dc07281d2f338bd9ec2508d3dc0245acee18
Jenorg <jenfermz44@gmail.com>
2025-06-05 21:18:54 -0500

    n for the world!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,9 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <main className="grid grid-cols-1 place-items-center h-full">
         <h2 className="">Bienvenidos al repositorio!</h2>
-        {<h1 className="text-4xl">Hola Mu</h1>}
+        {<h1 className="text-4xl">Hola Mun</h1>}
         <h1 className="text-4xl"></h1>
       </main>
     </div>
