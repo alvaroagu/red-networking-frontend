97382af40e89ee69985047aadcca4d735525e04f
jgarcia691 <108421222+jgarcia691@users.noreply.github.com>
2025-06-07 20:23:09 -0400

    Hello world + Form Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import AuthForm from "../components/form/AuthForm";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <main className="grid grid-cols-1 place-items-center h-full">
         <h2 className="">Bienvenidos al repositorio!</h2>
-        <h1 className="text-4xl">Hola Mund</h1>
+        <h1 className="text-4xl">Hola Mundo</h1>
+        <AuthForm /> {}
       </main>
     </div>
   );
